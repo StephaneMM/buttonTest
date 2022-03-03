f6ffb71a17076d261de24b1d26ee4b976c481ab0
Stephane Mbesse <stephane.mbesse@gmail.com>
2022-03-03 15:19:28 +0100

    feat:add button

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Button message="Hej" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from "./components/Button/Button.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Button,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <button>{{ message }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    message: {
+      default: "",
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  padding: 1rem;
+  font-family: Montserrat;
+  font-size: 1.125rem;
+  font-weight: 800;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #fff;
+  background-color: #00aaee;
+  border-radius: 1.25rem;
+  border: 0;
+  display: flex;
+  align-items: center;
+  align-content: center;
+  justify-content: center;
+  justify-items: center;
+  user-select: none;
+}
+
+button:enabled:active {
+  transform: scale(0.98) translateY(2px);
+  opacity: 0.9;
+  transition: transform 0.05s;
+}
+</style>
